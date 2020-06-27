625cc78cc6ab693d9d6424164171b014746f4359
Samuel <mbuthia.jsamuel@gmail.com>
2020-06-27 21:57:01 +0300

    Adding linter and fixing previous commit issues

--- src/JSmodules/data.js
+++ src/JSmodules/data.js
@@ -1,22 +1,15 @@
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeUSD: 5,
+    avgDailyIncomePopulation: 0.71,
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614,
+};
 
-
-export default function data (){
-    let inputData = {
-        region: {
-            name: "Africa",
-            avgAge: 19.7,
-            avgDailyIncomeUSD: 5,
-            avgDailyIncomePopulation: 0.71,
-        },
-
-        periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
-
-    }
-    return inputData;
-}
-
-console.log(data());
+export default data;
--- src/JSmodules/main.js
+++ src/JSmodules/main.js
@@ -1,25 +1,18 @@
+import './data';
 
-import data from './data';
-
-const covid19ImpactEstimator = (data) =>{
-    data = {};
-
-    impact = {
-        currentlyInfected = data.reportedCases * 10,
-        infectionsByRequestedTime = currentlyInfected*512
-
-    }
-
-    severeImpact = {
-        currentlyInfected = data.reportedCases*50,
-        infectionsByRequestedTime = currentlyInfected*512
-
-    }
-
-    return{
-        data,
-        impact,
-        severeImpact
-    }
-}
+const covid19ImpactEstimator = (data) => {
+  const impact = {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: this.currentlyInfected * 512,
+  };
+  const severeImpact = {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * 512,
+  };
+  return {
+    data,
+    impact,
+    severeImpact,
+  };
+};
 export default covid19ImpactEstimator;
