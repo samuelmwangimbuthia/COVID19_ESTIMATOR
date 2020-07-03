1ac621bee21e2bdc31a6f1bded53cb261ee02a8c
Samuel <mbuthia.jsamuel@gmail.com>
2020-07-03 20:43:00 +0300

    Revert "challenge 2"
    This reverts commit 153e9373aaae64a8287e4e47f3a3f05617c34f09.

--- src/JSmodules/main.js
+++ src/JSmodules/main.js
@@ -1,21 +1,12 @@
 import inputData from './data';
 
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, totalHospitalBeds } = data;
+  const { reportedCases } = data;
   const impact = {
     currentlyInfected: reportedCases * 10,
     infectionsByRequestedTime: (requestedTimeInDays) => {
       return (
         impact.currentlyInfected * 2 ** Math.floor(requestedTimeInDays / 3)
-      );
-    },
-    severeCasesByRequestedTime: (requestedTimeInDays) => {
-      return 0.15 * impact.infectionsByRequestedTime(requestedTimeInDays);
-    },
-    hospitalBedsByRequestedTime: (requestedTimeInDays) => {
-      return (
-        impact.severeCasesByRequestedTime(requestedTimeInDays) -
-        Math.floor(0.35 * ((100 / 95) * totalHospitalBeds))
       );
     },
   };
@@ -25,15 +16,6 @@ const covid19ImpactEstimator = (data) => {
       return (
         severeImpact.currentlyInfected *
         2 ** Math.floor(requestedTimeInDays / 3)
-      );
-    },
-    severeCasesByRequestedTime: (requestedTimeInDays) => {
-      return 0.15 * severeImpact.infectionsByRequestedTime(requestedTimeInDays);
-    },
-    hospitalBedsByRequestedTime: (requestedTimeInDays) => {
-      return (
-        severeImpact.severeCasesByRequestedTime(requestedTimeInDays) -
-        Math.floor(0.35 * ((100 / 95) * totalHospitalBeds))
       );
     },
   };
