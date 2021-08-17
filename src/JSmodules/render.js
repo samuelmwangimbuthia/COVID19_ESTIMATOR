773838ea56b44fb6bba5d69da9641cdd5e0aadce
mbuthia.jsamuel@gmail.com <mbuthia.jsamuel@gmail.com>
2021-08-17 19:39:33 +0300

    destructuring the dataSets array

--- src/JSmodules/render.js
+++ src/JSmodules/render.js
@@ -62,11 +62,11 @@ async function fetchData(x){
   //JSON.stringify(x);
    const{data} = x;
    const keysArray = Object.keys(data[1]);
-   const valuessArray = Object.values(data[1]);
+   const valuessArray = Object.values(data[3]);
    //console.log(data[1].date);
    //console.log(keysArray);
    //console.log(valuessArray);
-  return [valuessArray, keysArray];
+  return [keysArray, valuessArray];
 }
 main()
 .then(  function(params){
@@ -80,58 +80,38 @@ main()
   })
   .then(
       function (valuesArray) {
-        return valuesArray[0]
+        return valuesArray
       }
   
 )
 .then(
   function(dataSets){
-    createChart(dataSets)
+    createChart([dataSets])
   }
 )
 .catch(function(reason){
   console.log(reason)
 })
 
-function createChart(dataSets){
-const ctx = document.getElementById('myChart').getContext('2d');
-const xLabels = dataSets[0];
-const yLables = dataSets[1];
-const myChart = new Chart(ctx, {
+function createChart([dataSets]) {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const xLabels = dataSets[0];
+  const yLables = dataSets[1];
+  const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-       // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-       labels: xLabels,
-        datasets: [{
-            label: '# of Votes',
-            //data: [12,8,2,4,5,8],
-            data : yLables,
-            //data: main(),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      labels: xLabels,
+      datasets: [{
+        data: yLables,
+        borderWidth: 1
+      }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+      scales: {
+        y: {
+          beginAtZero: true
         }
+      }
     }
-});
+  });
 }
